524c253dec70f504d4ad90da9ec571c5bea0664d
Minh Nguyen <minhnc.com@gmail.com>
2022-11-19 10:58:07 +1100

    add twin.macro

---
+++ README.md
@@ -0,0 +1,1 @@
+## Discord-Inspired Dashboard with twin.macro - Tailwind with styled-components
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+};
